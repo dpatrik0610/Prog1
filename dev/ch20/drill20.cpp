ff041af5f3a19cbc159e4e36e980d6b9bd2015f6
qantanas <d.patrik0610@gmail.com>
2022-04-19 15:17:07 +0200

    Complete drill20.

--- dev/ch20/drill20.cpp
+++ dev/ch20/drill20.cpp
@@ -1,93 +1,111 @@
 #include <iostream>
-#include <stdexcept>
 #include <list>
 #include <array>
-#include <string>
 #include <vector>
+#include <string>
 #include <algorithm>
 
 using namespace std;
 
-template <typename C>
-void print(const C& c){
-	for (auto& i : c) {
-    	std::cout << i << " ";
-}
-	std::cout << std::endl;
-}
-
-template <typename C>
-void inc(C& c, int n){
-	for(auto& i : c){
-		i += n;
-	}
+template <typename type> 
+void print(const type& _type){ 
+	// As an input parameter we give a "type(name)" type reference.
+	for (auto& i : _type){ // rangefor to the end of _type reference.
+    	cout << i << " ";
+	};
+	cout << endl;
 }
 
-template<typename Iter1, typename Iter2>
-Iter2 orai_copy(Iter1 f1, Iter1 e1, Iter2 f2){
-	for(Iter1 p = f1; p!= e1; ++p){
-		*f2++=*p;
+template <typename type> 
+void increment(type& _type, int val){ 
+	// As an input parameter we give a "type(name)" type reference, and an increment value (val).
+	for(auto& i : _type){
+		i += val;
+	};
+}
+
+template<typename inType, typename outType> 
+// We give the input's and output's type.
+outType myCopy(inType _start, inType _end, outType _output){
+	// As an argument, we give an (inType) data type's start and end, and an (outType) output.
+	for (inType p = _start; p != _end; ++p) { // Increasing the index from the start index.
+		// We increase output first (to increase size of the output data type) and then give a value: first, the start's, after the increased one.
+		*_output++ = *p;
 	}
-	return f2;
+	return _output;
 }
 
 int main()
 try {
 	constexpr int size = 10;
+	int numbers[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
 
-	int arr[size];
-	for (int i = 0; i < size; ++i) arr[i] = i;
+	// This copy is a std function.
+	// 1. Define an array of 10 ints:
+	array<int, size> intArr;
+	myCopy(numbers, numbers + size, intArr.begin());
 
-	std::array<int, size> ai;
-	std::copy(arr, arr+size, ai.begin());
+	// 2. Define a vector:
+	vector<int> intVec(size);
+	myCopy(numbers, numbers + size, intVec.begin());
+	// 3. Define a list: 
+	list<int> intList(size);
+	myCopy(numbers, numbers + size, intList.begin());
 
-	std::vector<int> vi(size);
-	std::copy(arr, arr+size, vi.begin());
+	// 4. Define a 2nd of each data type:
+	array<int, size> intArrCopy = intArr;
+	vector<int> intVecCopy = intVec;
+	list<int> intListCopy = intList;
 
-	std::list<int> li(size);
-	std::copy(arr, arr+size, li.begin());
+	// Print them with a function to self check:
+	cout << "Array, Vector, List: " << endl;
+	print(intArrCopy);
+	print(intVecCopy);
+	print(intListCopy);
 
-	std::array<int, size> ai2 = ai;
-	std::vector<int> vi2 = vi;
-	std::list<int> li2 = li;
+	// 5. Increment each element in : array by 2 | vector by 3 | intListst by 5.
+	// I use a function for each.
+	increment(intArrCopy, 2);
+	increment(intVecCopy, 3);
+	increment(intListCopy, 5);
 
-	print(ai2);
-	print(vi2);
-	print(li2);
+	// Self check.
+	cout << "Incremented: " << endl;
+	print(intArrCopy);
+	print(intVecCopy);
+	print(intListCopy);
 
-	inc(ai2, 2);
-	inc(vi2, 3);
-	inc(li2, 5);
+	// 6. Write a copy function.
 
-	print(ai2);
-	print(vi2);
-	print(li2);
+	// 7. Use copy function to copy: array to vector, list to array.
+	copy(intArrCopy.begin(), intArrCopy.end(), intVecCopy.begin());
+	copy(intListCopy.begin(), intListCopy.end(), intArrCopy.begin());
+	cout << endl;
 
-	orai_copy(ai2.begin(), ai2.end(), vi2.begin());
-	orai_copy(li2.begin(), li2.end(), ai2.begin());
+	// Self check.
+	cout << "Copied Array: " << endl;
+	print(intArrCopy);
+	cout << "Copied Vector: " << endl;
+	print(intVecCopy);
+	cout << "List: " << endl;
+	print(intListCopy);
 
-	print(ai2);
-	print(vi2);
-	print(li2);
+	// Search for value 3 in the vector with an iterator.
+	int searchVal = 3;
+	//Iterator which points to an integer contained in a vector. (ch. 20.3 Sequences and iterators.)
+	vector<int>::iterator vecIter = find(intVecCopy.begin(), intVecCopy.end(), searchVal);
 
-	//Iterator which points to an integer contained in a vector.
-	std::vector<int>::iterator vit;
-	vit = std::find(vi2.begin(), vi2.end(), 3);
-
-	if(vit != vi2.end()){
-		std::cout << "Found at: " << std::distance(vi2.begin(), vit) << std::endl;
+	if(vecIter != intVecCopy.end()){
+		cout << "Value (" << searchVal << ") in vector found at: " << distance(intVecCopy.begin(), vecIter) << endl;
 	} else {
-		std::cout << "Not found" << std::endl;
-	}
-
-
-
+		cout << "Not found" << endl;
+	};
 }
-catch(std::exception& e){
-	std::cerr << "Exception: " << e.what() << '\n';
+catch(exception& e){
+	cerr << "Exception: " << e.what() << '\n';
 	return 1;
 }
 catch(...){
-	std::cerr << "Unknown exception\n";
+	cerr << "Unknown exception\n";
 	return 2;
 }
